f155b042543d51d32ec8e90479e85a38941d5463
Elvijs Leistmanis <s22leiselvi@vea.lv>
2024-04-10 11:10:25 +0300

    Add: reheapUp, reheapDown

--- src/datastr/MyHeap.java
+++ src/datastr/MyHeap.java
@@ -1,5 +1,6 @@
 package datastr;
 
+@SuppressWarnings("unchecked")
 public class MyHeap<T> {
 	private T[] heap;
 	private final int HEAP_DEFAULT_SIZE = 10;
@@ -48,7 +49,7 @@ public class MyHeap<T> {
 		
 		heap[counter] = element;
 		counter++;
-		//TODO reheapUp();
+		reheapUp(counter-1);
 	}
 	
 	public T dequeue() throws Exception {
@@ -57,12 +58,35 @@ public class MyHeap<T> {
 		T prioElement = heap[0];
 		heap[0] = heap[counter];
 		counter--;
-		//TODO reheapDown()
+		reheapDown(0);
 		return prioElement;
 	}
 	
-	
 	//TODO recursive print()
+	public void print2() throws Exception {
+		if(isEmpty()) throw new Exception("Array is empty and it "
+				+ "is not possible to print elements");
+	}
+
+	private void printRecursiveHelp(int index) {
+		T element = heap[index];
+		System.out.println("P: " + element + " ");
+		
+		int leftChildIndex = index * 2 + 1;
+		int rightChildIndex = index * 2 + 2; 
+		
+		if (leftChildIndex < counter) {
+			T leftChild = heap[leftChildIndex];
+			System.out.println("LC: " + leftChild + " [" + element + "]");
+			printRecursiveHelp(leftChildIndex);
+		}
+		else {
+			T rightChild = heap[rightChildIndex];
+			System.out.println("RC: " + rightChild + " [" + element + "]");
+			printRecursiveHelp(rightChildIndex);
+		}
+	}
+	
 	public void print() throws Exception {
 		
 		if(isEmpty()) throw new Exception("Array is empty and it "
@@ -74,11 +98,62 @@ public class MyHeap<T> {
 		System.out.println();
 	}
 	
-	
 	public void makeEmpty() {
 		counter = 0;
 		size = HEAP_DEFAULT_SIZE;
 		heap = (T[])new Object[size];
 		System.gc();	
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private void reheapUp(int index) {
+		int parentIndex = (index - 1) / 2;
+		if(index >= 0) {
+			T element = heap[index];
+			T parent = heap[parentIndex];
+			if( ((Comparable)(element)).compareTo(parent) == 1) {
+				swap(index, parentIndex);
+				reheapUp(parentIndex);
+			}
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private void reheapDown(int index) {
+		int leftChildIndex = index * 2 + 1;
+		int rightChildIndex = index * 2 + 2; 
+		
+		if (counter > leftChildIndex && counter > rightChildIndex) {
+			T element = heap[index];
+			T rightChild = heap[rightChildIndex];
+			T leftChild = heap[leftChildIndex];
+			
+			if ( ((Comparable)(leftChild)).compareTo(rightChild) == 1) {
+				if ( ((Comparable)(leftChild)).compareTo(element) == 1) {
+					swap(leftChildIndex, index);
+					reheapDown(leftChildIndex);
+				}
+			}
+			else {
+				if ( ((Comparable)(rightChild)).compareTo(element) == 1) {
+					swap(rightChildIndex, index);
+					reheapDown(rightChildIndex);
+				}
+			}
+		}
+		else if (leftChildIndex < counter && rightChildIndex >= counter) {
+			T element = heap[index];
+			T leftChild = heap[leftChildIndex];
+			
+			if ( ((Comparable)(leftChild)).compareTo(element) == 1) {
+				swap(leftChildIndex, index);
+			}
+		}
+	}
+	
+	private void swap(int index1, int index2) {
+		T temp = heap[index1];
+		heap[index1] = heap[index2];
+		heap[index2] = temp;
 	}
 }
